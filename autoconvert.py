2f57a7ecda3cf22a8739a259c6e832f6144fcf27
kabum <uu.kabum@gmail.com>
2010-05-15 14:40:09 +0200

    Adding initial functions and settings

--- autoconvert.py
+++ autoconvert.py
@@ -14,3 +14,61 @@
 # 
 # You should have received a copy of the GNU General Public License along
 # with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import pyinotify
+import ConfigParser
+import time
+
+class EventHandler(pyinotify.ProcessEvent):
+	def __init__(self, file):
+		''' initiates EventHandler with file where to write changes '''
+		pyinotify.ProcessEvent.__init__(self)
+		self.file = file
+
+	def process_IN_CLOSE_WRITE(self, event):
+		''' event handler for close file after writing event - append path '''
+		print 'close:\t', event.pathname
+		self.fileAppend(event.pathname)
+				
+	def process_IN_DELETE(self, event):
+		''' event handler for delete event - removes entries'''
+		print 'delete:\t', event.pathname
+		self.fileDeletePath(event.pathname)
+		
+	def fileAppend(self, path):
+		''' append line with current unixtimestamp and given path '''
+		self.fileDeletePath(path)
+		fileHandler = open(self.file, 'a')
+		fileHandler.write(str(time.time()) + '\t' + path + '\n')
+		fileHandler.close()
+	
+	def fileDeletePath(self, path):
+		''' deletes all lines with given path '''
+		tmp = []
+		fileHandler = open(self.file, 'r')
+		while True:
+			line = fileHandler.readline()
+			if line == '':
+				break
+			if not line.split('\t')[1][:-1] == path:
+				tmp.append(line)
+		fileHandler.close()
+		fileHandler = open(self.file, 'w')
+		fileHandler.writelines(tmp)
+		fileHandler.close()
+     
+# parse config file
+config = ConfigParser.ConfigParser()
+config.read('settings.conf')
+
+# settings
+watchingMask = pyinotify.IN_CLOSE_WRITE | pyinotify.IN_DELETE   
+watchingDir = config.get('Main', 'watchingDirectory')
+fooFile = config.get('Main', 'fooFile')
+
+# initialise
+watchManager = pyinotify.WatchManager()
+handler = EventHandler(fooFile)
+notifier = pyinotify.Notifier(watchManager, handler)
+watchManager.add_watch(watchingDir, watchingMask)
+notifier.loop()
